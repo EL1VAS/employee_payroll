0a66c9b5e2e6a01da234a2e1912ac6575d942254
Pythia <el1vas@yahoo.gr>
2026-04-15 11:08:05 +0200

    Wrote my model updated pom with jacarta dependency

---
+++ src/main/java/com/employee_payroll/model/Employee.java
@@ -0,0 +1,71 @@
+package com.employee_payroll.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.GenerationType;
+
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String position;
+
+    @Column(nullable = false, precision = 12, scale = 2)
+    private BigDecimal salary;
+
+    public Employee() {
+
+    }
+
+    public Employee (String name, String position, BigDecimal salary) {
+        this.name = name;
+        this.position = position;
+        this.salary = salary;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public BigDecimal getSalary() {
+        return salary;
+    }
+
+    public void setSalary(BigDecimal salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" + "id = " + id + ", name = " + name + ", salary = " + salary + "}";
+    }    
+}
